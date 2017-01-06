51bfbfdc362c537fb79c8a5a3239a1e9f6b5f333
CrowdHailer <peterhsaxton@gmail.com>
2017-01-06 15:09:49 +0000

    delete old keys

--- lib/signex.ex
+++ lib/signex.ex
@@ -1,11 +1,4 @@
 defmodule SignEx do
-  require SignEx.Signer
-  require SignEx.Verifier
-  require SignEx.Helper
-
-  def sign(message, private_key, public_key), do: SignEx.Signer.sign(message, private_key, public_key)
-  def verify(message, signature, public_key), do: SignEx.Verifier.verify(message, signature, public_key)
-  def signature_params(signature), do: SignEx.Helper.signature_params(signature)
 
   def secure(body, metadata, keypair) do
     metadata = Map.merge(metadata, %{"digest" => SignEx.HTTP.digest_header_for(body)})
