bebe85c32c10bd5aa0675969edbb2e6481c085e3
Peter Saxton <peterhsaxton@gmail.com>
2018-08-23 20:08:03 +0100

    Add dialyzer and fix typespecs (#19)
    * add application
    
    * update version

--- mix.exs
+++ mix.exs
@@ -1,27 +1,30 @@
 defmodule SignEx.Mixfile do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.trim
+  @version File.read!("VERSION") |> String.trim()
 
   def project do
-    [app: :signex,
-     version: @version,
-     elixir: "~> 1.3",
-     elixirc_paths: elixirc_paths(Mix.env),
-     deps: deps()]
+    [
+      app: :signex,
+      version: @version,
+      elixir: "~> 1.3",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      deps: deps()
+    ]
   end
 
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :public_key]]
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/keys"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
     [
       {:poison, "~> 2.0"},
-      {:plug, "~> 1.0"}
+      {:plug, "~> 1.0"},
+      {:dialyxir, "~> 1.0.0-rc.3", only: [:test, :dev], runtime: false}
     ]
   end
 end
